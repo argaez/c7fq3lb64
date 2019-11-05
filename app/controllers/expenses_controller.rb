9c0b0b6fb71c245567e7583052a660eceb5d3f9e
argaez <sargaezu@gmail.com>
2019-11-05 17:31:39 -0500

    Modificación Index2

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,11 +3,11 @@ class ExpensesController < ApplicationController
   	concept = params[:concept]
   	category_id = params[:category_id]
   	if !concept.blank? && !category_id.blank?
-    	@expenses = Expense.where("concept = '#{concept}' and category_id = #{category_id}").order("date DESC")
+    	@expenses = Expense.where("concept like '%#{concept}%' and category_id = #{category_id}").order("date DESC")
   	elsif concept.blank? && !category_id.blank?
   		@expenses = Expense.where("category_id = #{category_id}").order("date DESC")
   	elsif !concept.blank? && category_id.blank?
-  		@expenses = Expense.where("concept = '#{concept}'").order("date DESC")
+  		@expenses = Expense.where("concept like '%#{concept}%'").order("date DESC")
   	else
   		@expenses = Expense.order("date DESC")
   	end
